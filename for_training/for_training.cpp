59d978467e0c334c67eebf111c2d0c41c13c13ed
Расул <@DESKTOP-20CEVRG>
2022-09-04 18:39:57 +0300

    Добавьте файлы проекта.

---
+++ for_training/for_training.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+
+using namespace std;
+
+class Human
+{
+public:
+	Human(string _name, int _weight, int _high)
+	{
+		this->name = _name;
+		this->weight = _weight;
+		this->high = _high;
+	}
+
+
+
+private:
+	string name;
+	int weight, high;
+};
+
+
+int main()
+{
+	setlocale(0, "");
+
+	cout << "Салам\n";
+
+	return 0;
+}
